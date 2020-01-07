2f9a7b5634ae3f814625fb393372538714a5ddc8
Walter-Corbitt <heymile39@gmail.com>
2020-01-07 09:06:54 +0100

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/Meteo_Project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Meteo_Project.dir/main.cpp.o"
+  "Meteo_Project.pdb"
+  "Meteo_Project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Meteo_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
