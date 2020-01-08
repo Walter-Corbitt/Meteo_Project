df3d2d34523c29b238dceb0c78f4a7877944bc6b
Walter-Corbitt <heymile39@gmail.com>
2020-01-08 16:24:50 +0100

    Del Cmake

--- Courbes.cpp
+++ Courbes.cpp
@@ -6,6 +6,6 @@
 
 QEasingCurve easing(QEasingCurve::InOutQuad);
 
-for (qreal t = 0.0; t < 1.0; t += 0.1)
-qWarning() << "Effective progress" << t << "is" << easing.valueForProgress(t);
+//for (qreal t = 0.0; t < 1.0; t += 0.1)
+//qWarning() << "Effective progress" << t << "is" << easing.valueForProgress(t);
 
